45bea0ac37d018c6fab089d0e69bb4c2ba41f464
wogjs0911 <wogjs0911@naver.com>
2023-03-14 00:59:42 +0900

    [init] init modeul project

---
+++ ModeulPrj/src/test/java/com/modeul/web/ModeulPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modeul.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeulPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
